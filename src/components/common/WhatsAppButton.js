076d97f73dfd84cc690dc2d80920b5627c0aa642
Abdelruhman Abass <92057185+Abdelrhman-Abass@users.noreply.github.com>
2024-12-08 15:34:48 +0200

    add

--- src/components/common/WhatsAppButton.js
+++ src/components/common/WhatsAppButton.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const WhatsAppButton = () => {
   const phoneNumber = '+201094002482'; // Replace with the phone number you want to send the message to
-  const message = 'Hello, I am interested in your services!'; // The message to send
+  const message = 'Hello, I am interested in your services! '; // The message to send
   const encodedMessage = encodeURIComponent(message); // Encode the message to be URL safe
 
   const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
