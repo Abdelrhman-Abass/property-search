1a9cc77426b9d3818d1c3329294f5b6d40308f0a
Abdelruhman Abass <92057185+Abdelrhman-Abass@users.noreply.github.com>
2024-12-30 11:35:24 +0200

    fix the last at home hero slider

--- src/app/(Home)/[locale]/(home)/components/HomeHero.jsx
+++ src/app/(Home)/[locale]/(home)/components/HomeHero.jsx
@@ -12,14 +12,15 @@ const HomeHero = ({ data }) => {
       className="home-banner-style1 p0"
     >
       {/* Next.js Image as Background */}
-      {/* <Image
+
+      <Image
         src="/slider.webp"
         alt="Background Image"
-        width={1920} // Intrinsic width for large screens
-        height={1080} // Intrinsic height for large screens
+        width={1920}
+
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 100vw"
         quality={80}
         priority
-        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 100vw"
         style={{
           position: "absolute",
           width: "100%",
@@ -28,38 +29,9 @@ const HomeHero = ({ data }) => {
           objectPosition: "center",
           zIndex: -1,
         }}
-      /> */}
-      <Image
-        width={1920}
-        height={1080}
-        className="slider w-100 h-100 cover"
-        src="/slider.webp"
-        alt="banner"
-        priority
-        loading="eager" // Lazy-load for non-critical images
-        quality={80}
-        style={{
-          position: "absolute",
-          objectPosition: "center",
-          zIndex: -1,
-        }}
       />
-      <Image
-        width={250} // Set the width to a mobile-appropriate size
-        height={450} // Keep the height proportional to width
-        className="mobile-slider w-100 h-100 cover"
-        src="/slidermobile.webp"
-        alt="banner"
-        loading="lazy"
-        quality={80}
-        srcSet="/_next/image?url=%2Fslidermobile.webp&amp;w=320&amp;q=80 320w, /_next/image?url=%2Fslidermobile.webp&amp;w=480&amp;q=80 480w, /_next/image?url=%2Fslidermobile.webp&amp;w=640&amp;q=80 640w"
-        sizes="(max-width: 768px) 100vw"
-        style={{
-          position: "absolute",
-          objectPosition: "center",
-          zIndex: -1,
-        }}
-      />
+
+
       <div className="home-style1">
         <div className="container">
           <div className="row">
