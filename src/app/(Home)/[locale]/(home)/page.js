f758c7e159118fd5dd30fc6ebde02f9bd83696fc
Abdelruhman Abass <92057185+Abdelrhman-Abass@users.noreply.github.com>
2025-01-07 13:39:06 +0200

    split description

--- src/app/(Home)/[locale]/(home)/page.js
+++ src/app/(Home)/[locale]/(home)/page.js
@@ -8,6 +8,21 @@ import HomeHeroPage from "./@homeHero/page";
 import WhatsAppButton from "@/components/common/WhatsAppButton";
 
 
+export async function generateMetadata({params: { locale } }) {
+  const description = locale == "ar" ? "أفضل موقع وسيط عقاري في مصر. تصفح جميع المناطق والمجمعات السكنية والعقارات داخل مصر حسب السعر والموقع والمرافق للعثور على المكان المثالي." :  "Browse and monitor real estate prices in Egypt. Search areas, prices, and facilities to find the perfect place for you."
+
+  let seoResult = {
+    description,
+    openGraph: {
+      description,
+    },
+    twitter: {
+      description,
+    },
+  };
+
+  return seoResult;
+}
 
 const Home = async () => {
   const banners = await ApiGetter({ url: `/api/PageSections` });
