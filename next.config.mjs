4eb6df030e89d1d6f03fe97df3474352ea6e4d32
Abdelruhman Abass <92057185+Abdelrhman-Abass@users.noreply.github.com>
2025-01-19 11:07:58 +0200

    update lazy load

--- next.config.mjs
+++ next.config.mjs
@@ -14,32 +14,35 @@ const nextConfig = {
       },
     ],
   },
-  reactStrictMode: false, // Disable strict mode
-  output: 'standalone',
-  
-  devIndicators: {
-    autoPrerender: false,
+  compiler: {
+    removeConsole: false,
   },
-  
+  // reactStrictMode: false, // Disable strict mode
+  // output: 'standalone',
 
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/api/:path*`,
-      },
-    ];
-  },
-  
-  webpack(config) {
-    // Example of customizing Webpack if needed
-    config.resolve.fallback = {
-      fs: false, // Example if you need to disable specific Node.js modules (in case of SSR issues)
-    };
-    return config;
-  },
-  
-  
+  // devIndicators: {
+  //   autoPrerender: false,
+  // },
+
+
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/:path*",
+  //       destination: `${process.env.NEXT_PUBLIC_BASE_URL}/api/:path*`,
+  //     },
+  //   ];
+  // },
+
+  // webpack(config) {
+  //   // Example of customizing Webpack if needed
+  //   config.resolve.fallback = {
+  //     fs: false, // Example if you need to disable specific Node.js modules (in case of SSR issues)
+  //   };
+  //   return config;
+  // },
+
+
 };
 
 export default withNextIntl(nextConfig);
