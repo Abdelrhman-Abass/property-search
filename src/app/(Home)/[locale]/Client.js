7bcfd3a9d88ba814365849bb45b3446ebb31c9f2
Abdelruhman Abass <92057185+Abdelrhman-Abass@users.noreply.github.com>
2025-01-19 10:39:14 +0200

    test removing aos

--- src/app/(Home)/[locale]/Client.js
+++ src/app/(Home)/[locale]/Client.js
@@ -1,10 +1,10 @@
-"use client";
+"use server";
 import ScrollToTop from "@/components/common/ScrollTop";
-import Aos from "aos";
-import "aos/dist/aos.css";
+// import Aos from "aos";
+// import "aos/dist/aos.css";
 import "../../../../public/scss/main.scss";
 import "rc-slider/assets/index.css";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import "../../../../public/style.css";
 import { DataProvider } from "@/context";
 
@@ -12,18 +12,18 @@ if (typeof window !== "undefined") {
   import("bootstrap");
 }
 
-export default function Client({ children }) {
-  const [isMounted, setIsMounted] = useState(false);
+export default async function Client({ children }) {
+  // const [isMounted, setIsMounted] = useState(false);
 
-  useEffect(() => {
-    Aos.init({
-      duration: 900,
-      once: true,
-    });
-    setIsMounted(true);
-  }, []);
+  // useEffect(() => {
+  //   Aos.init({
+  //     duration: 900,
+  //     once: true,
+  //   });
+  //   setIsMounted(true);
+  // }, []);
 
-  if (!isMounted) return null;
+  // if (!isMounted) return null;
 
   return (
     <DataProvider>
