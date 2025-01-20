c81b67ae7a2c191ed3e7afce451171fd396dc935
Abdelruhman Abass <92057185+Abdelrhman-Abass@users.noreply.github.com>
2025-01-20 16:54:48 +0200

    tracking in home hero

--- src/app/(Home)/[locale]/(home)/components/HomeHero.jsx
+++ src/app/(Home)/[locale]/(home)/components/HomeHero.jsx
@@ -24,8 +24,8 @@ const HomeHero = ({ data }) => {
       
       
 
-      <h1  className="d-none">Luxury Real Estate in Egypt البحث عن منزل أحلامك</h1>
-      <h2 className="d-none">Find Your Dream Villas and Apartments لدينا مجموعة كبيرة من الوحدات التى يمكنك معرفة معلومات عنها. </h2>
+      {/* <h1  className="d-none">Luxury Real Estate in Egypt البحث عن منزل أحلامك</h1> */}
+      {/* <h2 className="d-none">Find Your Dream Villas and Apartments لدينا مجموعة كبيرة من الوحدات التى يمكنك معرفة معلومات عنها. </h2>
       <div className="home-style1">
         <div className="container">
           <div className="row">
@@ -37,7 +37,7 @@ const HomeHero = ({ data }) => {
         <Link href="#top-area" aria-label="mouse scroll">
           <div className="mouse_scroll animate-up-4"></div>
         </Link>
-      </div>
+      </div> */}
     </section>
   );
 };
